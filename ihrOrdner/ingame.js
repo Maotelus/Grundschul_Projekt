55485b66fcbaf85ac66602c120480b87cf105832
KargoG <30510699+KargoG@users.noreply.github.com>
2017-07-28 19:35:36 +0200

    ImportOfEverythingIHave
    hab hier so ziemlich alles wichtige drinn

---
+++ ihrOrdner/ingame.js
@@ -0,0 +1,20 @@
+
+$(function(){
+
+	setUpDosenstand();
+});
+
+function setUpDosenstand()
+{
+	$("body").css(
+	{
+		"backgroundImage" : "url('bilder/Dosenstand.png')",
+		"background-repeat" : "no-repeat"
+	});
+
+	var dose1 = document.createElement("div");
+	dose.
+	dose1.css({"backgroundImage" : "url('bilder/JahrmarkDose_1.png')"});
+
+	$("body").append(dose1); //"<div></div>");
+}
